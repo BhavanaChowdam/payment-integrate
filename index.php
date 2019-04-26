b9452cb795cd5a9d28f19a76f760b28eb59f02e3
Bhavana Chowdam <48642329+BhavanaChowdam@users.noreply.github.com>
2019-04-26 23:16:36 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>SPARKS FOUNDATION</title>
+
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+   <style> body{
+ margin:0;
+ padding: 0;
+ font-family:sans-serif;
+ background-image: linear-gradient(rgba(0,0,0,0.5),rgba(0,0,0,0.3)),url("im.jpg");
+ background-size: cover;
+ color:pink; 
+}
+.amount
+{
+color:black;
+}
+</style>
+</head>
+
+  <body> <form action="order.php" method="post">
+    <div class="container">
+
+      
+        <h1>THE SPARKS FOUNDATION</h1>
+
+        <p class="lead">DONATE HERE</p>
+<div class="col-md-4">
+<center>
+<h4>HELP US PROVIDE BRIGHT FUTURE TO THE NEEDED</h4>
+<h4>Enter Amount: <input type="number" class="amount" name="amount"></h4>
+<button class="btn btn-success"> DONATE NOW </button></center>
+
+</div>
+      
+      
+    </div> <!-- /container -->
+</form>
+
+  </body>
+</html>
